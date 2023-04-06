079b8a83538e980171c0b11514cf137e5cb294fb
JanaKoduje <jjanule@gmail.com>
2023-04-06 20:06:51 +0200

    component selectedSeat

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,19 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { JourneyPicker } from '../JourneyPicker';
 import { JourneyDetail } from '../JourneyDetail';
+import { SelectedSeat } from '../SelectedSeat';
 
 export const Home = () => {
   const [journey, setJourney] = useState(null);
-  
+
   const handleJourneyChange = (detail) => {
-        setJourney(detail)
-       }
-  
-  
+    setJourney(detail)
+  }
+
+
   return (
     <main>
-      <JourneyPicker onJourneyChange={handleJourneyChange}/>
-      { journey === null ? null : <JourneyDetail journey={journey}/>}
+      <JourneyPicker onJourneyChange={handleJourneyChange} />
+      {journey === null ? null :
+        <>
+          <JourneyDetail journey={journey} />
+          <SelectedSeat number={journey.autoSeat} />
+        </>
+      }
     </main>
   );
 }
