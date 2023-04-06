ba8c0c3d9b2b621446f2501a758583e2f428fdf2
JanaKoduje <jjanule@gmail.com>
2023-04-06 18:05:21 +0200

    basic router

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <RouterProvider router={router} />
     <Footer />
   </>
 );
+
+
+const router = createBrowserRouter([
+  {
+    path: '/', 
+    element: <Home />
+  },
+  {
+    path: '/reservation',
+    element: <Reservation />
+  }
+])
