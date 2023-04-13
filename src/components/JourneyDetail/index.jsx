e2a657d675abae3bf0f0da8a3d65741925eac7fa
JanaKoduje <jjanule@gmail.com>
2023-04-13 17:17:25 +0200

    komponenta Reservation

--- src/components/JourneyDetail/index.jsx
+++ src/components/JourneyDetail/index.jsx
@@ -8,7 +8,13 @@ export const JourneyDetail = ({ journey }) => {
         <div className="journey-detail container">
             <h2>Podrobnosti cesty</h2>
             <div className="stops">
-                {journey.stops.map((stop) => <BusStop key={stop.code} name={stop.name} station={stop.station} time={stop.time} />)}
+                {journey.stops.map((stop) =>
+                    <BusStop
+                        key={stop.code}
+                        name={stop.name}
+                        station={stop.station}
+                        time={stop.time}
+                    />)}
             </div>
         </div>
     )
--- src/components/Reservation/index.jsx
+++ src/components/Reservation/index.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { API_BASE_URL } from "../..";
+import './style.css';
 
 export const Reservation = () => {
+
+    const { reservationId } = useParams();
+
+    const [reservation, setReservation] = useState(null)
+
+    const NoReservation = () => {
+        return (
+            <div className="reservation container">
+                <h2>Rezervace nenalezena</h2>
+            </div>
+        )
+    }
+
+    useEffect(() => {
+        fetch(`${API_BASE_URL}/reservation/?id=${reservationId}`)
+            .then((response) => response.json())
+            .then((data) => setReservation(data.results))
+
+    }, [])
+
+
     return (
-    <>
-    <h2>Rezervace</h2>
-       
-    </>)
-} 
+        <>
+            {reservation ?
+                <div className="reservation container">
+                    <h2>Vaše e-jízdenka č. HAQBAQASf7M</h2>
+                    <div className="reservation__body">
+                        <div className="reservation__headings">
+                            <p>Datum cesty:</p>
+                            <p>Odjezd:</p>
+                            <p>Příjezd:</p>
+                            <p>Sedadlo:</p>
+                        </div>
+                        <div className="reservation__info">
+                            <p>{reservation.date}</p>
+                            <p>{reservation.fromCity.name}, {reservation.fromCity.time}</p>
+                            <p>{reservation.toCity.name}, {reservation.toCity.time}</p>
+                            <p>{reservation.seatNumber}</p>
+                        </div>
+                    </div>
+                </div>
+                : <NoReservation />}
+        </>)
+}
+
